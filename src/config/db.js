a9ecd2252207c79ba9823b27e3018abd48eb3989
Ravi Shankar Singh <91066307+ravvviii@users.noreply.github.com>
2025-10-23 11:01:19 +0530

    db setup to alutus

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (uri) => {
-  if (!uri) throw new Error("MONGO_URI missing");
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(uri, { dbName: uri.split("/").pop() });
-  console.log("✅ Mongo connected");
+  if (!uri) throw new Error("❌ MONGO_URI missing in .env file");
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Mongo connected");
+  } catch (err) {
+    console.error("❌ Mongo connection error:", err);
+    process.exit(1);
+  }
 };
